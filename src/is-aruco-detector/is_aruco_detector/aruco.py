d264b4fdd3cabf5cdbbe86510e7533f7a0f96f96
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-03-08 15:54:36 -0300

    :sparkles: Add zipkin support to is-aruco-detector

--- src/is-aruco-detector/is_aruco_detector/aruco.py
+++ src/is-aruco-detector/is_aruco_detector/aruco.py
@@ -9,32 +9,36 @@ from is_msgs.common_pb2 import Tensor, DataType
 from is_msgs.image_pb2 import Image, ObjectAnnotations
 from is_msgs.camera_pb2 import CameraCalibration, FrameTransformations
 
-from is_wire.core import Channel, Subscription, Logger, Message
+from opencensus.trace.span import Span
+from opencensus.ext.zipkin.trace_exporter import ZipkinExporter
+
+from dateutil import parser as dp
+from is_wire.core import Channel, Subscription, Logger, Message, Tracer
 
 from is_aruco_detector.conf.options_pb2 import ArUcoSettings
 
 
 class ArUcoDetector:
-    def __init__(self, settings: ArUcoSettings, channel: Channel, subscription: Subscription):
+    def __init__(self, settings: ArUcoSettings, channel: Channel, subscription: Subscription, exporter: ZipkinExporter):
         self.logger = Logger("ArUco")
         self.channel = channel
         self.settings = settings
+        self.exporter = exporter
         self.subscription = subscription
         self.subscription.subscribe("CameraGateway.*.Frame")
         self.re_topic = re.compile(r'CameraGateway.(\d+).Frame')
-
         self.dictionary = cv2.aruco.getPredefinedDictionary(settings.dictionary)
         self.parameters = cv2.aruco.DetectorParameters()
         self.detector = cv2.aruco.ArucoDetector(self.dictionary, self.parameters)
 
     @staticmethod
-    def to_array(image: Image) -> np.ndarray:
+    def image2array(image: Image) -> np.ndarray:
         buffer = np.frombuffer(image.data, dtype=np.uint8)
         array = cv2.imdecode(buffer, flags=cv2.IMREAD_COLOR)
         return array
 
     @staticmethod
-    def to_np(tensor: Tensor) -> np.ndarray:
+    def tensor2array(tensor: Tensor) -> np.ndarray:
         if len(tensor.shape.dims) != 2 or tensor.shape.dims[0].name != 'rows':
             return np.array([])
         shape = (tensor.shape.dims[0].size, tensor.shape.dims[1].size)
@@ -49,13 +53,13 @@ class ArUcoDetector:
         return np.array([])
 
     @staticmethod
-    def to_tensor(array: np.ndarray) -> Tensor:
+    def array2tensor(array: np.ndarray) -> Tensor:
         tensor = Tensor()
         rows = tensor.shape.dims.add()
         rows.size = array.shape[0]
         rows.name = "rows"
         cols = tensor.shape.dims.add()
-        cols.size = array.shape[0]
+        cols.size = array.shape[1]
         cols.name = "cols"
         if array.dtype == np.int32:
             tensor.type = DataType.Value('INT32_TYPE')
@@ -72,7 +76,7 @@ class ArUcoDetector:
         return tensor
 
     def detect(self, image: Image) -> ObjectAnnotations:
-        array = self.to_array(image)
+        array = self.image2array(image)
         detections, ids, _ = self.detector.detectMarkers(array)
         annotations = ObjectAnnotations()
         for i, corners in enumerate(detections):
@@ -93,9 +97,8 @@ class ArUcoDetector:
         sx = annotations.resolution.width / calibration.resolution.width
         sy = annotations.resolution.height / calibration.resolution.height
         scale = np.array([[sx, 0.0, 0.0], [0.0, sy, 0.0], [0.0, 0.0, 1.0]])
-        intrinsic = np.dot(scale, self.to_np(tensor=calibration.intrinsic))
-        distorsion = self.to_np(tensor=calibration.distortion)
-
+        intrinsic = np.dot(scale, self.tensor2array(tensor=calibration.intrinsic))
+        distorsion = self.tensor2array(tensor=calibration.distortion)
         transformations = FrameTransformations()
         for index in range(len(annotations.objects)):
             object = annotations.objects[index]
@@ -112,7 +115,9 @@ class ArUcoDetector:
                                  [x3, y3],
                                  [x4, y4]]], dtype=np.float32)
             if object.id not in self.settings.lengths.keys():
-                return
+                self.logger.critical(
+                    "Detected aruco with ID={}, but not found in configuration \
+                        its length".format(object.id))
             marker_length = self.settings.lengths[object.id]
             rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(
                 corners,
@@ -125,11 +130,16 @@ class ArUcoDetector:
             mat = np.hstack((rmat, tvec))
             mat = np.vstack((mat, np.array([[0, 0, 0, 1]])))
             transformation = transformations.tfs.add()
-            transformation.tf.CopyFrom(self.to_tensor(array=mat))
+            transformation.tf.CopyFrom(self.array2tensor(array=mat))
             # claramente é gambiarra, em python 'from' é palavra reservada
             setattr(transformation, 'from', 100 + object.id)
             setattr(transformation, 'to', calibration.id)
         return transformations
+
+    @staticmethod
+    def span_duration_ms(span: Span) -> float:
+        dt = dp.parse(span.end_time) - dp.parse(span.start_time)
+        return dt.total_seconds() * 1000.0
 
     def run(self,
             message: Message,
@@ -141,18 +151,27 @@ class ArUcoDetector:
             camera_id = int(match.group(1))
             if camera_id not in calibrations:
                 return camera_id
-        image = message.unpack(Image)
-        annotations = self.detect(image=image)
-
-        message = Message(content=annotations)
-        message.topic = "ArUco.{}.Detection".format(camera_id)
-        self.channel.publish(message=message)
-
-        transformations = self.localize(
-            annotations=annotations,
-            calibration=calibrations[camera_id],
+        tracer = Tracer(
+            exporter=self.exporter,
+            span_context=message.extract_tracing(),
         )
-        if transformations is not None:
-            message = Message(content=transformations)
-            message.topic = "ArUco.{}.FrameTransformations".format(camera_id)
-            self.channel.publish(message=message)
+        span = None
+
+        with tracer.span(name="detect_and_localize") as _span:
+            image = message.unpack(schema=Image)
+            annotations = self.detect(image=image)
+            message_obs = Message(content=annotations)
+            message_obs.topic = "ArUco.{}.Detection".format(camera_id)
+            message_obs.inject_tracing(span=_span)
+            transformations = self.localize(
+                annotations=annotations,
+                calibration=calibrations[camera_id],
+            )
+            message_ann = Message(content=transformations)
+            message_ann.topic = "ArUco.{}.FrameTransformations".format(camera_id)
+            message_ann.inject_tracing(span=_span)
+            span = _span
+        self.channel.publish(message=message_obs)
+        self.channel.publish(message=message_ann)
+        took_ms = round(self.span_duration_ms(span), 2)
+        self.logger.info("Publish image, took_ms={}".format(took_ms))
--- src/is-aruco-detector/is_aruco_detector/calibration.py
+++ src/is-aruco-detector/is_aruco_detector/calibration.py
@@ -1,16 +1,20 @@
 import time
 
-from is_wire.core import StatusCode
-from is_wire.core import Channel, Subscription, Message, Logger
-from is_msgs.camera_pb2 import GetCalibrationReply, GetCalibrationRequest
+from typing import Dict, Union
 
+from is_wire.core import StatusCode
+from is_wire.core import Channel, Subscription, Message, Logger, Tracer
+
+from opencensus.ext.zipkin.trace_exporter import ZipkinExporter
+from is_msgs.camera_pb2 import GetCalibrationReply, GetCalibrationRequest, CameraCalibration
 
 class CalibrationFetcher:
 
-    def __init__(self, channel: Channel, subscription: Subscription):
+    def __init__(self, channel: Channel, subscription: Subscription, exporter: ZipkinExporter):
         self.logger = Logger("CalibrationFetcher")
         self.channel = channel
         self.subscription = subscription
+        self.exporter = exporter
 
         self.fetch_interval = 5
         self.timeout = time.perf_counter() + self.fetch_interval
@@ -26,18 +30,25 @@ class CalibrationFetcher:
         if now >= self.timeout:
             self.timeout = now + self.fetch_interval
             if len(self.camera_ids) > 0:
-                calib_request = GetCalibrationRequest()
-                calib_request.ids.extend(self.camera_ids)
-                request = Message(
-                    content=calib_request,
-                    reply_to=self.subscription,
-                )
-                request.topic = "CalibrationServer.GetCalibration"
+                tracer = Tracer(exporter=self.exporter)
+                with tracer.span(name="request") as _span:
+                    calib_request = GetCalibrationRequest()
+                    calib_request.ids.extend(self.camera_ids)
+                    request = Message(
+                        content=calib_request,
+                        reply_to=self.subscription,
+                    )
+                    request.topic = "CalibrationServer.GetCalibration"
+                    request.inject_tracing(_span)
                 self.channel.publish(message=request)
                 self.logger.info("Requesting calibrations, ids={}", self.camera_ids)
 
-    def run(self, message: Message):
-        if message.has_correlation_id:
+    def run(self, message: Message) -> Dict[int, CameraCalibration]:
+        tracer = Tracer(
+            exporter=self.exporter,
+            span_context=message.extract_tracing(),
+        )
+        with tracer.span(name="reply"):
             if message.status.code == StatusCode.OK:
                 reply = message.unpack(GetCalibrationReply)
                 for calibration in reply.calibrations:
